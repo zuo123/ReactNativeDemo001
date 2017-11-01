c8d4e9692c45de7e2eb491b124715063edf1f926
zuo123 <zuo12138@163.com>
2017-11-01 16:55:15 +0800

    第一次提交

---
+++ app/src/main/java/com/cnbs/rn1030/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.cnbs.rn1030;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * author: zuo
+ * date: 2017/10/30 17:02
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
